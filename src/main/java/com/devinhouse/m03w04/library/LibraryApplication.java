364ddc3d29b61228c2f6837f0fc562e58b81bb14
lfogawa <lfogawa0@gmail.com>
2024-02-25 12:29:15 -0300

    first commit

---
+++ src/main/java/com/devinhouse/m03w04/library/LibraryApplication.java
@@ -0,0 +1,13 @@
+package com.devinhouse.m03w04.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devinhouse/m03w04/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devinhouse.m03w04.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
